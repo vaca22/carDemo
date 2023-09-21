4868549955a461680bd9dce5508faf19e03ed279
vaca22 <wsmslgh@gmail.com>
2023-09-21 15:05:04 +0800

    local ip

--- app/src/main/java/com/example/cardemo/MainActivity.kt
+++ app/src/main/java/com/example/cardemo/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.cardemo
 import android.car.Car
 import android.car.hardware.CarPropertyValue
 import android.car.hardware.property.CarPropertyManager
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cardemo.databinding.ActivityMainBinding
+import com.example.cardemo.utils.NetUtils
+import com.example.cardemo.utils.PathUtil
 import com.example.cardemo.view.Converter
 import com.example.cardemo.view.Er1WaveUtil
 import com.example.cardemo.view.FullEcgAmpAdapter
@@ -20,6 +25,10 @@ import com.example.cardemo.view.WavePara.drawTask
 import com.example.cardemo.view.WavePara.offerTask
 import com.example.cardemo.view.WavePara.updateSignal
 import com.example.cardemo.view.WaveView
+import kotlinx.coroutines.MainScope
+import java.io.File
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.util.Date
 import java.util.Timer
 
@@ -46,14 +55,18 @@ class MainActivity : AppCompatActivity() {
             0
         }
     }
-
-    var mode = 1;
+    private fun intToIp(paramInt: Int): String {
+        return ((paramInt.and(255)).toString() + "." + (paramInt.shr(8).and(255)) + "." + (paramInt.shr(16).and(255)) + "."
+                + (paramInt.shr(24).and(255)))
+    }
+    var ecgArray:ArrayList<Short> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        PathUtil.initVar(this)
         getScreenInfo()
 
 
@@ -65,13 +78,21 @@ class MainActivity : AppCompatActivity() {
             binding.leadStatus.text = it
         }
 
+        var status = 0
+
         ecgData.observe(this) {
+            if(status==1){
+                for (k in 0 until it.size) {
+                    ecgArray.add(it[k].toShort())
+                }
+            }
+
             for (k in 0 until it.size) {
                 WavePara.waveDataX.offer(Er1WaveUtil.byteTomV(it[k]))
             }
         }
 
-        var status = 0
+
 
         binding.start.text = "开始录制"
         binding.status.text = "未录制"
@@ -81,14 +102,36 @@ class MainActivity : AppCompatActivity() {
                 status = 1
                 binding.start.text = "停止录制"
                 binding.status.text = "正在录制"
+                ecgArray.clear()
             } else {
                 status = 0
                 binding.start.text = "开始录制"
                 binding.status.text = "未录制"
+                val byteBuffer= ByteBuffer.allocate(2*ecgArray.size).order(ByteOrder.LITTLE_ENDIAN)
+                for(k in 0 until ecgArray.size){
+                   byteBuffer.putShort(ecgArray[k])
+                }
+                val timeString=java.text.SimpleDateFormat("yyyyMMddHHmmss").format(java.util.Date())+".dat"
+                val file=File(PathUtil.getPathX(timeString))
+                file.writeBytes(byteBuffer.array())
+                Thread{
+                    try {
+                        val str=NetUtils.postFile("http://vaca.tpddns.cn:9889/ecg_file", file);
+                        MainScope().run {
+                            Toast.makeText(this@MainActivity,"上传成功",Toast.LENGTH_LONG).show()
+                        }
+                    }catch (e:Exception){
+                        MainScope().run {
+                            Toast.makeText(this@MainActivity,"上传失败",Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
             }
 
         }
-
+        val wifiManager =getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val gate=intToIp(wifiManager.dhcpInfo.ipAddress)
+        binding.status.text = gate
 
         initAmp()
         initAndroidCar()
